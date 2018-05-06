f78c530819bcfd550d9ba229cd4cb4b021e1ecf8
Dallas Scott <epicosity@gmail.com>
2018-05-05 22:03:04 -0500

    second significant commit
    too many changes to describe

--- Scripts/tetris.js
+++ Scripts/tetris.js
@@ -1,7 +1,112 @@
 ﻿var matrix;//6 columns x 12 rows
-var selector, rowCount, columnCount, ctx;
+var selector, rowCount, columnCount, ctx, canvasWidth, canvasHeight, canvasSize, blockSize;
 var max = 6;
 var spriteSheet = new Image();
+var xMoveAmt = .2;
+var yMoveAmt = .2;
+var timer;
+var interval = 1000 / 60;
+var images = {};
+var URLs = { image1: 'Sprites.png' };
+var block;
+
+function Timer() {
+    this.last = null;
+    this.elapsed = 0;
+}
+
+Timer.prototype = {
+    tick: function (now) {
+        this.last = this.last || now
+        this.elapsed = now - this.Last
+    }
+}
+
+function Sprite(options) {
+    this.size = blockSize;
+    this.spriteSize = 7;
+    this.blockType = options.blockType;
+    this.interval = 1000 / options.fps;
+    this.row = options.row;
+    this.column = options.column;
+    this.now = new Date().getTime();
+    this.XPos = 0;
+    this.YPos = 0;
+}
+
+Sprite.prototype = {
+    clear: function (canvasX, canvasY){
+        ctx.clearRect(canvasX, canvasY, this.size, this.size);
+    },
+    draw: function (canvasX, canvasY) {
+        this.determineXY();
+        ctx.drawImage(document.getElementById("sprites"),
+            this.pixelsLeft, this.pixelsTop,
+            this.spriteSize, this.spriteSize,
+            canvasX, canvasY,
+            this.size, this.size);
+    },
+    determineXY: function () {
+        switch (this.blockType) {
+            case 0:
+                this.pixelsLeft = 0;
+                this.pixelsTop = 0;
+                break;
+            case 1:
+                this.pixelsLeft = 8;
+                this.pixelsTop = 0;
+                break;
+            case 2:
+                this.pixelsLeft = 16;
+                this.pixelsTop = 0;
+                break;
+            case 3:
+                this.pixelsLeft = 0;
+                this.pixelsTop = 8;
+                break;
+            case 4:
+                this.pixelsLeft = 8;
+                this.pixelsTop = 8;
+                break;
+            case 5:
+                this.pixelsLeft = 16;
+                this.pixelsTop = 8;
+                break;
+            case 6:
+                this.pixelsLeft = 0;
+                this.pixelsTop = 16;
+                break;
+            case 7:
+                this.pixelsLeft = 8;
+                this.pixelsTop = 16;
+                break;
+            case 8:
+                this.pixelsLeft = 16;
+                this.pixelsTop = 16;
+                break;
+        }
+    }
+}
+
+function Block(row, column, blockType) {
+    this.row = row;
+    this.column = column;
+    this.blockType = blockType;
+    this.sprite = new Sprite({ blockType: blockType, row: row, column: column, fps: 4 });
+}
+
+function render(now) {
+    requestAnimationFrame(render);
+    timer.tick(now)
+    if (timer.elapsed >= interval) {
+        /*block.Sprite.Clear(block.Sprite.XPos, block.Sprite.YPos)
+        block.Sprite.YPos += yMoveAmt;
+        block.Sprite.Draw(block.Sprite.XPos, block.Sprite.YPos);*/
+        var then = timer.elapsed % interval;
+        timer.last = now - then;
+        // draw sprite
+    }
+}
 
 function initializeMatrix(columns, rows) {
     var initialMatrix = [];
@@ -13,6 +118,7 @@ function initializeMatrix(columns, rows) {
         for (var j = 0; j < rows; j++) {
             var newBlock = new Block(j, i, Math.floor(Math.random() * (max + 1)));
             initialMatrix[i][j] = newBlock;
+            newBlock.sprite.draw(j, i);
         }
     }
     return initialMatrix;
@@ -26,7 +132,7 @@ function buildArrayFromColumns(row) {
     return blockArray;
 }
 
-function checkArray(blockArray) {
+function cleanArray(blockArray) {
     var deleteArray = [];
     for (var i = 1; i < blockArray.length-1; i++) {
         if (blockArray[i].blockType == blockArray[i - 1].blockType) {
@@ -37,15 +143,22 @@ function checkArray(blockArray) {
     return deleteArray;
 }
 
-function checkMatrix() {
+function deleteBlocks(matchingBlocks) {
+    for (var j = 0; j < matchingBlocks.length; j++) {
+        matchingBlocks[j].blockType = max + 1;
+        matchingBlocks[j].sprite.clear(1, 1);
+    }
+}
+
+function cleanMatrix() {
     for (var i = 0; i < columnCount; i++) {
-        var checkedColumn = checkArray(matrix[i]);
-        deleteBlocks(checkedColumn);
+        var cleanedRow = cleanArray(matrix[i]);
+        deleteBlocks(cleanedRow);
     }
     for (var j = 0; j < rowCount; j++) {
         var rowArray = buildArrayFromColumns(j);
-        var checkedRow = checkArray(rowArray);
-        deleteBlocks(checkedRow);
+        var cleanedColumn = cleanArray(rowArray);
+        deleteBlocks(cleanedColumn);
     }
 }
 
@@ -55,19 +168,16 @@ function switchBlocks(selector) {
     selector[1] = tempHolder;
 }
 
-function deleteBlocks(matchingBlocks) {
-    for (var j = 0; j < matchingBlocks.length; j++) {
-        matchingBlocks[j].blockType = max + 1;
-    }
-}
-
 function dropBlockDown(block) {
     if (block.row == 0 || matrix[block.column][block.row - 1].blockType != max + 1) {
+        newBlock.sprite.draw(1, 1);
         return;
     }
     else {
         var selector = [matrix[block.column][block.row], matrix[block.column][block.row - 1]];
-        switchBlocks(selector)
+        switchBlocks(selector);
+        newBlock.sprite.clear(1, 1);
+        newBlock.sprite.draw(1, 1);
         dropBlockDown(block);
     }
 }
@@ -122,17 +232,20 @@ function pause() { }
 function stop() { }
 
 $(document).ready(function () {
-    matrix = initializeMatrix(6, 12);
+    timer = new Timer();
+    //matrix = initializeMatrix(6, 12);
+    rowCount = 6;
+    columnCount = 12;
 
-    selector = [matrix[0][0], matrix[1][0]];
-    checkMatrix();
-    logCurrentMatrixState();
+    //selector = [matrix[0][0], matrix[1][0]];
+    //checkMatrix();
+    //logCurrentMatrixState();
 });
 
 $(window).load(function () {
     createCanvas();
-    dropAllBlocks();
-    logCurrentMatrixState();
+    //dropAllBlocks();
+    //logCurrentMatrixState();
 })
 
 /*$(document).keydown(function (event) {
@@ -152,120 +265,22 @@ $(window).load(function () {
     //dropAllBlocks();
 });*/
 
-var timer = new Timer();
-
-function render (now) {
-  requestAnimationFrame(render);
-  timer.tick(now);
-  if (timer.elapsed >= 250) {
-    timer.last = now - (timer.elapsed % 250);
-    // draw jewel
-  }
-}
-
-Timer.prototype = {
-    tick: function (now) {
-      this.last = this.last || now
-      this.elapsed = now - this.last
-    }
-}
-
 function createCanvas() {
     var canvas = document.getElementById("game");
+    canvas.width = window.innerWidth;///110;
+    canvas.height = window.innerHeight;///130;
     ctx = canvas.getContext("2d");
-    var blockSprite = new Sprite(0);
-    blockSprite.draw(9, 15);
-    //var img = document.getElementById("sprites");
-    //ctx.drawImage(img, 10, 10);
+
+    canvasWidth = canvas.width;
+    canvasHeight = canvas.height;
+    canvasSize = canvasWidth * canvasHeight;
+    var temp = rowCount * columnCount;
+    blockSize = canvasSize / (temp * temp * 7);
+    block = new Block(0, 0, 3);
+    matrix = initializeMatrix(rowCount, columnCount);
+    requestAnimationFrame(render);
 }
 
-function Sprite(options) {
-    this.size = 50;
-    this.spriteSize = 8;
-    this.blockType = options.blockType;
-    this.interval = 1000 / options.fps;
-    this.timer = new Timer();
-    this.row = options.row;
-    this.column = options.column;
-}
-
-Sprite.prototype = {
-    draw: function (canvasX, canvasY) {
-        this.determineXY();
-        ctx.drawImage(document.getElementById("sprites"),
-            this.pixelsLeft, this.pixelsTop,
-            this.spriteSize, this.spriteSize,
-            canvasX, canvasY,
-            this.size, this.size);
-    },
-    determineXY: function () {
-        switch (this.blockType) {
-            case 0:
-                this.pixelsLeft = 0;
-                this.pixelsTop = 0;
-                break;
-            case 1:
-                this.pixelsLeft = 8;
-                this.pixelsTop = 0;
-                break;
-            case 2:
-                this.pixelsLeft = 16;
-                this.pixelsTop = 0;
-                break;
-            case 3:
-                this.pixelsLeft = 0;
-                this.pixelsTop = 8;
-                break;
-            case 4:
-                this.pixelsLeft = 8;
-                this.pixelsTop = 8;
-                break;
-            case 5:
-                this.pixelsLeft = 16;
-                this.pixelsTop = 8;
-                break;
-            case 6:
-                this.pixelsLeft = 0;
-                this.pixelsTop = 16;
-                break;
-            case 7:
-                this.pixelsLeft = 8;
-                this.pixelsTop = 16;
-                break;
-            case 8:
-                this.pixelsLeft = 16;
-                this.pixelsTop = 16;
-                break;
-        }
-    },
-    render: function (now) {
-        requestAnimationFrame(this.render);
-        this.timer.tick(now)
-        if (this.timer.elapsed >= this.interval) {
-          var then = this.timer.elapsed % this.interval
-          this.timer.last = now - then
-          // draw sprite
-          this.draw(9,13);
-        }
-    }
-}
-
-function Block(row, column, blockType){
-    this.row = row;
-    this.column = column;
-    this.blockType = blockType;
-    this.sprite = new Sprite({blockType: blockType, row: row, column: column, fps: 4});
-}
-
-// callback -- after all images are loaded 
-function draw(images){ 
-    // or whatever you want in your callback 
-    context.drawImage(images.image1, 10, 10, 50, 50);
-    context.drawImage(images.image2, 10, 100, 50, 50);
-}
-
-var images = {};
-var URLs = { image1: 'Sprites.png' };
 /*LoadImages(URLs, draw);
 
 function LoadImages(URLs, callback) {
@@ -285,7 +300,7 @@ function LoadImages(URLs, callback) {
         };
         images[url].src = "~/Images/Sprites.png";
     }
-}*/
+}
 
 function logCurrentMatrixState() {
     var matrixAsString;
@@ -299,4 +314,4 @@ function logCurrentMatrixState() {
         matrix += "\n";
     }
     console.log(matrixAsString);
-}
+}*/
